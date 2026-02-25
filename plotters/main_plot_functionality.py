4a45ff7e3b7d6add898cb20fb7e3ea9284569670
hgp297 <hgp@berkeley.edu>
2026-02-24 19:10:53 -0800

    refactor(plotters): moved plotting under plotters/main_plot_functionality
    - Removed plotModel_PBEE_recovery.py (moved under plotters for future
      archiving)
    - Added plot_results() function to plotters.main_plot_functionality

--- plotters/main_plot_functionality.py
+++ plotters/main_plot_functionality.py
@@ -1,3 +1,41 @@
+def plot_results(outputs_dir, p_gantt=50):
+    """
+    Plot Functional Recovery Plots For a Single Model and Single Intensity
+    
+    Parameters
+    ----------
+    output dir: string
+        Directory of outputs
+    p_gantt: float
+        percentile of functional recovery time to plot for the gantt chart
+        default: 50 = 50th percentile of functional recovery time (median)
+    
+    """
+    import os
+    import json
+    import pandas as pd
+    
+    # from plotters import main_plot_functionality
+    # Plot Functional Recovery Plots For a Single Model and Single Intensity
+    
+    ## Define User inputs
+    
+    # Load systems information
+    systems = pd.read_csv(os.path.join(os.path.dirname(__file__), os.pardir, 'src', 'atc138', 'data', 'systems.csv'))
+    systems = systems['name']
+    
+    # outputs will save to a directory with this name
+    plot_dir = outputs_dir +'/plots' # Directory where the plots will be saved
+    
+    ## Import Packages
+    
+    ## Load Assessment Output Data
+    f = open(os.path.join(outputs_dir, 'recovery_outputs.json'))
+    functionality= json.load(f)
+    
+    ## Create plot for single intensity assessment of PBEE Recovery
+    main_plot_functionality(functionality, plot_dir, p_gantt, systems)
+
 def main_plot_functionality(functionality, save_dir, p_gantt, systems):
     '''Plot function and occupancy loss and recovery at for a single model at a 
     single intensity levels
